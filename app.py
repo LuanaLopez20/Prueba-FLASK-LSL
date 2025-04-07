9b07f53005471a13598f4ec11015f07052c0188d
LuanaLopez20 <luanaslopez@outlook.com>
2025-04-07 09:54:43 -0300

    Agregue nuevas rutas con argumentos

--- app.py
+++ app.py
@@ -10,3 +10,17 @@ def hello():
 @app.route("/chau")
 def bye():
     return "<p>Chau, MUNDO!</p>"
+
+@app.route("/Saludar/por-nombre/<string:nombre>")
+def sxn (Nombre):
+    return "<p>Hola{Nombre}<p>"
+
+@app.route("/Sumar/<int:n1/<int:n2>")
+def sum (n1,n2):
+    return f"<p>{n1} + {n2} = {s}<p>"
+
+@app.route("/Tirar/dados/<int:caras>")
+def dados (Caras):
+    from random import randint
+    n=randint(Caras)
+    return f"<p>Tire un dado de{Caras} caras, salio {n}<p>"
